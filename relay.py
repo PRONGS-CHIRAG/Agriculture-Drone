fdad84d6e501b15143a5b34d35a2839d438547c7
Prongs Potter <31549342+PRONGS-CHIRAG@users.noreply.github.com>
2019-05-19 13:01:06 +0530

    Add files via upload

---
+++ relay.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+channel = 18
+
+# GPIO setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.OUT)
+
+
+def motor_on(pin):
+    GPIO.output(pin, GPIO.HIGH)  # Turn motor on
+
+
+def motor_off(pin):
+    GPIO.output(pin, GPIO.LOW)  # Turn motor off
+
+
+if __name__ == '__main__':
+    try:
+        motor_on(channel)
+        time.sleep(1)
+        
+        GPIO.cleanup()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
